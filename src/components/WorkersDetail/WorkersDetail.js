c1aa05953a8882f4e742a3243da04be4350895d5
rcejudo <rocio.cejudo.soronellas@gmail.com>
2022-01-11 14:44:11 +0100

    Add workers detail page

---
+++ src/components/WorkersDetail/WorkersDetail.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+const WorkersDetail = () => {
+  const { id } = useParams();
+  const [workerDetail, setWorkerDetail] = useState([]);
+
+  useEffect(() => {
+    CallApi();
+  }, []);
+
+  const CallApi = async () => {
+    const data = await fetch(
+      `https://2q2woep105.execute-api.eu-west-1.amazonaws.com/napptilus/oompa-loompas/${id}`
+    );
+    const users = await data.json();
+    console.log("u", users);
+    setWorkerDetail(users);
+  };
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <img src={workerDetail.image} />
+        </div>
+        <div className="col">
+          <div>
+            <h3>{workerDetail.first_name + "  " + workerDetail.last_name}</h3>
+            <h4>{workerDetail.gender === "F" ? "Female" : "Male"}</h4>
+            <h4>{workerDetail.profession}</h4>
+          </div>
+          <div>
+            <h5>{workerDetail.description}</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WorkersDetail;
